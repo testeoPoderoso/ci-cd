69e63d545f5ff7bb3ccdfe62300eeb9b5c7d2f46
pablodg12 <pablo.ibarras@alumnos.usm.cl>
2020-12-03 00:29:49 -0300

    otro mas

--- deploy_step.sh
+++ deploy_step.sh
@@ -21,5 +21,4 @@ echo ${BUCKET}
 echo ${FILENAME}
 
 
-sam build --template-file cloudformationstep.yaml && sam package --output-template-file packaged.yaml --template-file cloudformationstep.yaml --s3-bucket ${S3_BUCKET} \
-&& sam deploy --capabilities CAPABILITY_NAMED_IAM --stack-name ${STACK_NAME}
+sam build --template-file cloudformationstep.yaml && sam package --output-template-file packaged.yaml --s3-bucket ${S3_BUCKET} && sam deploy --template-file packaged.yaml --capabilities CAPABILITY_NAMED_IAM --stack-name ${STACK_NAME}
