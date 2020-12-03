98f1a6ba6ccaa435ecd11f4b0e8dc1fb82c03e1d
MatheusTesseracto <matheus.pinto@tesseracto.com>
2020-12-02 22:10:44 -0300

    pablo senpai

---
+++ deploy_step.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+: "${STACK_NAME:=$1}"
+: "${S3_BUCKET:=$2}"
+
+if [[ -z ${STACK_NAME} ]] ; then
+  echo "No stackname was provided."
+  echo "Use: sh deploy.sh <STACK_NAME> <S3_BUCKET>"
+  exit 2
+fi
+
+if [[ -z ${S3_BUCKET} ]] ; then
+  echo "No S3 bucket defined, using 'euc-test-brasil."
+  S3_BUCKET="euc-test-brasil"
+fi
+
+FILENAME=$(echo $RANDOM.${STACK_NAME} | openssl dgst -sha1 | sed 's/^.* //')
+BUCKET="s3://$S3_BUCKET/$STACK_NAME/$FILENAME"
+
+echo ${BUCKET}
+echo ${FILENAME}
+
+
+sam build --template-file cloudformationstep.yaml && sam package --output-template-file packaged.yaml --template-file cloudformationstep.yaml --s3-bucket ${S3_BUCKET} \
+&& sam deploy --template-file packaged.yaml --capabilities CAPABILITY_NAMED_IAM --stack-name ${STACK_NAME}
---
+++ step_funtion/.aws-sam/build/ProcesamientoHander1/send_message_module.py
@@ -0,0 +1,11 @@
+import requests
+
+
+def telegram_bot_sendtext(bot_message):
+    bot_token = "1314762060:AAFvTjHCuMOSGH4cFnU9CzXc6sN0cG3jhlM"
+    bot_chatID = '-457266763'
+    send_text = ('https://api.telegram.org/bot' + bot_token +
+                 '/sendMessage?chat_id=' + bot_chatID +
+                 '&parse_mode=Markdown&text=' + bot_message)
+    response = requests.get(send_text)
+    return response.json()
