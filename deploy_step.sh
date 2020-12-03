b4a450762c3b07adaa253e2b9bbfc2d85cc3370e
pablodg12 <pablo.ibarras@alumnos.usm.cl>
2020-12-02 23:13:20 -0300

    check /Users/pabloibarra/Desktop/cicd

--- deploy_step.sh
+++ deploy_step.sh
@@ -21,5 +21,5 @@ echo ${BUCKET}
 echo ${FILENAME}
 
 
-sam package --output-template-file packaged.yaml --template-file cloudformationstep.yaml --s3-bucket ${S3_BUCKET} \
+sam build --template-file cloudformationstep.yaml && sam package --output-template-file packaged.yaml --template-file cloudformationstep.yaml --s3-bucket ${S3_BUCKET} \
 && sam deploy --template-file packaged.yaml --capabilities CAPABILITY_NAMED_IAM --stack-name ${STACK_NAME}
